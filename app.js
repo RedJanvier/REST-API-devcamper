3e9ac11ed3348a9889bc408253004a68fe81cc92
RedJanvier <janvierntwali@gmail.com>
2019-10-28 13:18:35 +0200

    added [morgan npm] as a logger and configured [dotenv] in [app.js]

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 const bootcampsRouter = require('./routes/bootcamps.js');
 
+dotenv.config({ path: './config/config.env' });
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcampsRouter);
 
