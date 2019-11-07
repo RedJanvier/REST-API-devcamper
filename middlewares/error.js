5666257d9c92c62c7a40d397dd5d3be413b5f77b
RedJanvier <janvierntwali@gmail.com>
2019-11-07 03:59:32 +0200

    [middlewares/error.js] added duplicate key and validation errors catch

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,9 +4,22 @@ const errorHandler = (err, req, res, next) => {
   // eslint-disable-next-line node/no-unsupported-features/es-syntax
   let error = { ...err, message: err.message };
 
+  // Unkcaught Mongoose Id
   if (err.name === 'CastError') {
     const message = `Resource not found of id ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res
